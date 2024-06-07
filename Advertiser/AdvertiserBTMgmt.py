ac860d6a29730dbb90f7fee38f0f378bc166c169
J0EK3R <J0EK3R@gmx.net>
2024-06-07 16:28:40 +0200

    JK: switched back to publischer loop

--- Advertiser/AdvertiserBTMgmt.py
+++ Advertiser/AdvertiserBTMgmt.py
@@ -35,7 +35,7 @@ class AdvertiserBTMgmt(Advertiser) :
 
         return
 
-    def AdvertisementStop(self):
+    def AdvertisementStop_(self):
         """
         stop bluetooth advertising
         """
@@ -59,7 +59,7 @@ class AdvertiserBTMgmt(Advertiser) :
         self._advertisementTable.clear()
         return
 
-    def AdvertisementSet(self, identifier: str, manufacturerId: bytes, rawdata: bytes):
+    def AdvertisementSet_(self, identifier: str, manufacturerId: bytes, rawdata: bytes):
         """
         Set Advertisement data
         """
@@ -88,9 +88,55 @@ class AdvertiserBTMgmt(Advertiser) :
 
         return
 
+    def AdvertisementStop(self):
+        """
+        stop bluetooth advertising
+        """
+
+        self._ad_thread_Run = False
+        if(self._ad_thread is not None):
+            self._ad_thread.join()
+            self._ad_thread = None
+            self._isInitialized = False
+
+        self._advertisementTable.clear()
+
+        hcitool_args_0x08_0x000a = self.BTMgmt_path + ' rm-adv 1'
+
+        subprocess.run(hcitool_args_0x08_0x000a + ' &> /dev/null', shell=True, executable="/bin/bash")
+
+        if (self._tracer is not None):
+            self._tracer.TraceInfo('AdvertiserBTMgmnt.AdvertisementStop')
+            self._tracer.TraceInfo(hcitool_args_0x08_0x000a)
+
+        return
+
+    def AdvertisementSet(self, identifier: str, manufacturerId: bytes, rawdata: bytes):
+        """
+        Set Advertisement data
+        """
+
+        advertisementCommand = self.BTMgmt_path + ' add-adv -d ' + self._CreateTelegramForBTMgmmt(manufacturerId, rawdata) + ' --general-discov 1'
+        self._ad_thread_Lock.acquire(blocking=True)
+        self._advertisementTable[identifier] = advertisementCommand
+        self._ad_thread_Lock.release()
+
+        if(not self._ad_thread_Run):
+            self._ad_thread = threading.Thread(target=self._publish)
+            self._ad_thread.daemon = True
+            self._ad_thread.start()
+            self._ad_thread_Run = True
+
+        if (self._tracer is not None):
+            self._tracer.TraceInfo('AdvertiserBTMgmnt.AdvertisementSet')
+
+        return
+
     def _publish(self):
         if (self._tracer is not None):
-            self._tracer.TraceInfo('AdvertiserBTMgmt._publish')
+            self._tracer.TraceInfo('AdvertiserBTMgmnt._publish')
+
+        lastSetAdvertisementCommand = None
 
         while(self._ad_thread_Run):
             try:
@@ -104,26 +150,29 @@ class AdvertiserBTMgmt(Advertiser) :
                 timeSlot = 1 / repetitionsPerSecond / max(1, len(copy_of_advertisementTable))
 
                 for key, advertisementCommand in copy_of_advertisementTable.items():
-                    # stopp publishing?
+                    # stop publishing?
                     if(not self._ad_thread_Run):
                         return
 
                     timeStart = time.time()    
-                    subprocess.run(advertisementCommand + ' &> /dev/null', shell=True, executable="/bin/bash")
 
-                    if(not self._isInitialized):
-                        # hcitool_args_0x08_0x0006 = self.BTMgmt_path + ' -i hci0 cmd 0x08 0x0006 A0 00 A0 00 03 00 00 00 00 00 00 00 00 07 00'
-                        # hcitool_args_0x08_0x000a = self.BTMgmt_path + ' -i hci0 cmd 0x08 0x000a 01'
+                    if (lastSetAdvertisementCommand != advertisementCommand):
+                        subprocess.run(advertisementCommand + ' &> /dev/null', shell=True, executable="/bin/bash")
+                        lastSetAdvertisementCommand = advertisementCommand
 
-                        # subprocess.run(hcitool_args_0x08_0x0006 + ' &> /dev/null', shell=True, executable="/bin/bash")
-                        # subprocess.run(hcitool_args_0x08_0x000a + ' &> /dev/null', shell=True, executable="/bin/bash")
+                        if(not self._isInitialized):
+                            # hcitool_args_0x08_0x0006 = self.HCITool_path + ' -i hci0 cmd 0x08 0x0006 A0 00 A0 00 03 00 00 00 00 00 00 00 00 07 00'
+                            # hcitool_args_0x08_0x000a = self.HCITool_path + ' -i hci0 cmd 0x08 0x000a 01'
 
-                        # if (self._tracer is not None):
-                        #     self._tracer.TraceInfo(str(hcitool_args_0x08_0x0006))
-                        #     self._tracer.TraceInfo(str(hcitool_args_0x08_0x000a))
-                        #     self._tracer.TraceInfo()
-                        
-                        self._isInitialized = True
+                            # subprocess.run(hcitool_args_0x08_0x0006 + ' &> /dev/null', shell=True, executable="/bin/bash")
+                            # subprocess.run(hcitool_args_0x08_0x000a + ' &> /dev/null', shell=True, executable="/bin/bash")
+
+                            # if (self._tracer is not None):
+                            #     self._tracer.TraceInfo(str(hcitool_args_0x08_0x0006))
+                            #     self._tracer.TraceInfo(str(hcitool_args_0x08_0x000a))
+                            #     self._tracer.TraceInfo()
+                            
+                            self._isInitialized = True
 
                     timeEnd = time.time()    
                     timeDelta = timeEnd - timeStart
